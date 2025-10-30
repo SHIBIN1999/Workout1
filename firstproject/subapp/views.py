cc7644abc1531709104e1f0875ec1ed4c821d13d
shahir <shibinshahir7@gmail.com>
2025-10-30 16:05:50 +0530

    {{name}} is jijar format is dictionary key passing value

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("hello")
+def jijarformat(request):
+    return render(request,'jijar.html',{'name':'kerala'})
